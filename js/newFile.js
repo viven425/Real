5c28a3b387dec173700b6f1d8af101c4e7b2672b
vivien425 <vivian9898@gmail.com>
2025-05-13 18:24:32 +0800

    5.13

--- js/newFile.js
+++ js/newFile.js
@@ -9,3 +9,28 @@ setInterval(() => {
 }, 5000);
 
 
+//图片放大缩写//
+// import { useEffect } from 'react'
+// // import $ form 'Jquery' 为了控制Jquery ，需要按照成功才可以。 
+// import './App.css'
+// export default function App() {
+//     useEffect(()=>{    一定要在这里面写是Rreact 里面的
+//         //当话术碰到图片区域时，放大图片
+//         $ ('.cssAnim1').on('mouseover',function(){
+//             $(this).addClass('imgScale');
+//         })
+
+//         //当话术碰到图片区域时，还原图片
+//          $ ('.cssAnim1').on('mouseout',function(){
+//             $(this).removeClass('imgScale');
+//         })
+//     },[])
+
+
+//     return (
+//         <div>
+//             <a href="#" className='cssAnim1'>
+//             <img src="./img/App.jsx" alt="" /></a>
+//         </div>
+//     )
+// }
