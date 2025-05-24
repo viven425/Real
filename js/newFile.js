7443788cb57408a7ec0ddcdfa6ee81a624c66229
vivien425 <yinvivian9898@gmail.com>
2025-05-24 17:07:43 +0800

    5.24

--- js/newFile.js
+++ js/newFile.js
@@ -8,29 +8,26 @@ setInterval(() => {
   items[current].classList.add("active");
 }, 5000);
 
+//汉堡按鈕的
 
-//图片放大缩写//
-// import { useEffect } from 'react'
-// // import $ form 'Jquery' 为了控制Jquery ，需要按照成功才可以。 
-// import './App.css'
-// export default function App() {
-//     useEffect(()=>{    一定要在这里面写是Rreact 里面的
-//         //当话术碰到图片区域时，放大图片
-//         $ ('.cssAnim1').on('mouseover',function(){
-//             $(this).addClass('imgScale');
-//         })
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburger = document.querySelector('.hamburger-icon');
+    const body = document.body;
+    const menu = document.querySelector('.menu');
 
-//         //当话术碰到图片区域时，还原图片
-//          $ ('.cssAnim1').on('mouseout',function(){
-//             $(this).removeClass('imgScale');
-//         })
-//     },[])
+    if (hamburger) {
+        hamburger.addEventListener('click', () => {
+            body.classList.toggle('menu-open');
+        });
+    }
 
-
-//     return (
-//         <div>
-//             <a href="#" className='cssAnim1'>
-//             <img src="./img/App.jsx" alt="" /></a>
-//         </div>
-//     )
-// }
+    if (menu) {
+        menu.querySelectorAll('.menu-link-item').forEach(item => {
+            item.addEventListener('click', () => {
+                if (body.classList.contains('menu-open')) {
+                    body.classList.remove('menu-open');
+                }
+            });
+        });
+    }
+});
